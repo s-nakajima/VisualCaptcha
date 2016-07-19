5dd9db6b2aaf92058929dac628fbfe5d768f35dd
s-nakajima <nakajimashouhei@gmail.com>
2016-07-19 10:52:11 +0900

    ルーティングの見直し https://github.com/NetCommons3/NetCommons3/issues/309 https://github.com/NetCommons3/NetCommons/pull/284

---
+++ Config/routes.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Users routes configuration
+ *
+ * @author Shohei Nakajima <nakajimashouhei@gmail.com>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+Router::connect(
+	'/visual_captcha/:controller/:action/*',
+	array('plugin' => 'visual_captcha')
+);
